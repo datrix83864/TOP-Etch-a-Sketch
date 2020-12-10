fdd2cb003ea25f2876bf7f67d0f9b686093444f7
Dalton <daltonhwes@gmail.com>
2020-12-09 23:33:55 -0800

    Add README.md and basic website

---
+++ JavaCode.js
@@ -0,0 +1,40 @@
+const grid = document.querySelector('#container')
+grid.style.display = 'grid';
+//grid.style.
+
+makeGrid(16);
+function makeGrid(sideSize){
+    grid.textContent = '';
+    for(let j = 0; j < sideSize; j++){
+        for(let i = 0; i < sideSize; i++){
+            const square = document.createElement('div');
+            square.style.color = 'purple';
+            square.style.display = 'grid';
+            square.style.gridColumn = i + 1;
+            square.style.gridRow = j + 1;
+            square.style.minHeight = '10px';
+            square.style.backgroundColor = 'pink';
+            //square.textContent = i;
+            //square.style.gridAutoColumns = minmax(10, auto);
+            grid.appendChild(square);
+        }
+    }
+}
+
+const dip = document.querySelectorAll('div');
+
+dip.forEach((div) => {
+    div.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = 'black';
+    });
+
+})
+
+const reset = document.querySelector('#recreate');
+reset.addEventListener('click', (e) => {
+    let newSize = prompt("How many per side?");
+    if(newSize > 100){
+        newSize = 100;
+    }
+    makeGrid(newSize)
+})
